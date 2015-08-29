1a4545212a0af98223b0ca3602e2a287a7479ce7
Mar Leonard Lopez <marlen3291@gmail.com>
2015-08-29 02:32:24 -0400

    Added comments to file codes to explain functionality and what  each line of code is used for

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,15 +1,26 @@
+
+//path to the users server controller
 var users = require('../../app/controllers/users.server.controller');
 
+//
 module.exports = function(app){
+    //Makes the route and url for the users page and includes methods
 	app.route('/users')
+	//Creates users
 	   .post(users.create)
+	//Retrieves a list of users
 	   .get(users.list);
-
+	   
+    //Makes the route andurl for the user's id page and includes its methods
 	app.route('/users/:userId')
+	//Gets specific user's information
 	   .get(users.read)
+	//Updates the specific user's information field
 	   .put(users.update)
+	//Deletes the specific user
 	   .delete(users.delete)
 	;
-
+    
+    //Turns the :userId into the user's userByID field in the database 
 	app.param('userId', users.userByID);
 };
